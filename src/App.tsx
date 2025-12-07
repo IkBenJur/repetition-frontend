c0cd221d91e19c0d83ffdec8af61549f778c1457
IkBenJur <jursemail@gmail.com>
2025-12-07 20:39:55 +0100

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import './App.css'
-
 function App() {
-
   return (
-    <>
-      <h1>Repetition</h1>
-    </>
+    <div className='bg-gray-800'>
+      <h1 className="text-3xl text-red-800 font-bold underline">Repetition</h1>
+    </div>
   )
 }
 
