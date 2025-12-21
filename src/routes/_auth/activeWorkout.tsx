0eb18331df96c51839063700587d8d00ad8211e8
IkBenJur <jursemail@gmail.com>
2025-12-21 20:36:22 +0100

    Better sizing

--- src/routes/_auth/activeWorkout.tsx
+++ src/routes/_auth/activeWorkout.tsx
@@ -26,7 +26,7 @@ function RouteComponent() {
   const { data } = useSuspenseActiveUserWorkout();
 
   return (
-    <div className="max-w-4xl mx-auto p-6">
+    <div className="container mx-auto p-6">
       {data ? (
         <UserWorkoutForm userWorkout={data} />
       ) : (
